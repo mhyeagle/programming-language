728c67384e63e96d2cb1bb28410101159d9ab647
luna <tianyashuibin@163.com>
2017-03-12 23:06:50 +0800

    cpp/struct_test.cc

---
+++ cpp/struct_test.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+typedef struct BinNode {
+	int value;
+	BinNode* lchild;
+	BinNode* rchild;
+} BNODE_S;
+
+int main() {
+	BNODE_S node;
+
+}
