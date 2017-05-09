d37f01560b121fc25a92221d077ea6529b541c3f
mhyeagle <tianyashuibin@163.com>
2017-05-09 01:05:55 -0700

    python: sys_test.py

---
+++ python/learn_python/test/sys_test.py
@@ -0,0 +1,15 @@
+import sys
+import os
+
+print __file__
+os_path_abspath = os.path.abspath(__file__)
+print "os.path.abspath: %s" %(os_path_abspath)
+dirname, filename = os.path.split(os_path_abspath)
+print "dirname: %s, filename: %s" %(dirname, filename)
+os_path_pardir = os.path.pardir
+print "os_path_abspath: %s" %(os_path_pardir)
+
+print "-----------------------"
+home_dir = os.path.join(os.path.split(os.path.realpath(__file__))[0],os.path.pardir)
+print "home_dir: %s" %(home_dir)
+
