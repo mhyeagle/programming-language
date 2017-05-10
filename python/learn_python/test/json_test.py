3a41554463e3a58791c9773af3554e6866d2b160
mhyeagle <tianyashuibin@163.com>
2017-05-09 18:56:09 -0700

    json_test.py

---
+++ python/learn_python/test/json_test.py
@@ -0,0 +1,15 @@
+import json
+
+data = {'a':1, 'b':2, 'x':3, 'c':4}
+
+json_text = json.dumps(data, sort_keys=True, indent=4)
+
+print "json_text: " 
+print json_text
+
+data_str = '{"a":1, "c":2, "b":3}'
+json_obj = json.loads(data_str)
+print "a: %d" %(json_obj["a"])
+print "c: %d" %(json_obj["c"])
+
+
