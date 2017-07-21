e9c585737685578bafe452aac239328c6a5d3832
mhyeagle <tianyashuibin@163.com>
2017-07-21 17:47:47 +0800

    python: sys.exit()

--- python/test/sys_test.py
+++ python/test/sys_test.py
@@ -1,5 +1,8 @@
+#-*-coding:UTF-8-*-
+
 import sys
 import os
+import traceback
 
 print __file__
 os_path_abspath = os.path.abspath(__file__)
@@ -18,4 +21,21 @@ platform = sys.platform
 print "platform: %s"%(platform)
 flag = platform.startswith("win")
 print flag
+
+print "-----------3------------"
+print "使用sys.exit()语句会抛出一个异常"
+try:
+    sys.exit(0)
+except SystemExit:
+    print "catch SystemExit"
+except:
+    print "catch other except:"
+    traceback.print_exc()
+
+print "-----------4------------"
+#try:
+#    if 1 == 1:
+#        return
+#except:
+#    print "catch except"
 
