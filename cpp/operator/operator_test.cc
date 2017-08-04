abf178643048b87ed2829ad241134c4a8a93b928
mhyeagle <tianyashuibin@163.com>
2017-08-04 12:54:46 +0800

    cpp: operator overload

--- cpp/operator/operator_test.cc
+++ cpp/operator/operator_test.cc
@@ -12,6 +12,8 @@ public:
     ~A(){}
 
     A operator+(const A& a);
+
+    friend A operator-(const A& a, const A& b);
     
 
 public:
@@ -20,6 +22,10 @@ public:
 
 inline A A::operator+(const A& a) {
     return A(real+a.real, imag+a.imag);
+}
+
+A operator-(const A& a, const A& b) {
+    return A(a.real-b.real, a.imag-b.imag);
 }
 
 
@@ -31,6 +37,12 @@ int main() {
     c = a1 + a2;
 
     cout << c.real << "\t" << c.imag << endl;
+
+    //******friend function******
+    A d(5, 5.0);
+    A e;
+    e = a2 - d;
+    cout << e.real << "\t" << e.imag << endl;
     
     return 0;
 }
