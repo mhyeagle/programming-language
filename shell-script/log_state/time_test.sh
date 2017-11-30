8e12577f7e3a4fd2669d8de6aebfad2e43bfb03a
mhyeagle <tianyashuibin@163.com>
2017-11-30 21:36:35 +0800

    shell time_test

--- shell-script/log_state/time_test.sh
+++ shell-script/log_state/time_test.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-echo $1
-echo $2
-echo $3
+start_hour="2017113018"
 
+num_hour=0
+
+now=`date +%Y%m%d%H`
+echo "now:" $now
+
+log_hour=$now
+while [[ $log_hour != $start_hour ]]
+do
+    echo $log_hour
+    num_hour=$(($num_hour + 1))
+    echo $num_hour
+
+    log_hour=`date --date="$num_hour hour ago" +%Y%m%d%H`
+done
+echo $log_hour
+
+echo "******************"
+while [[ $num_hour != 0 ]]
+do
+    log_hour=`date --date="$num_hour hour ago" +%Y%m%d%H`
+    echo $log_hour
+    num_hour=$(($num_hour - 1))
+done
 
